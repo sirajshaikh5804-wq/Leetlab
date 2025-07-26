24b1e7d5f675bf36f5db5eebf4da973c379421e1
sirajshaikh-dev <ss1425306@gmail.com>
2025-07-26 12:35:29 +0530

    implement user registration with email and password, including JWT token generation and cookie setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,9 +3,66 @@ import jwt from 'jsonwebtoken'
 
 import {db} from '../lib/db.js'
 import { UserRole } from '../generated/prisma/index.js'
-import { UserRole } from '../generated/prisma/index.js'
 
-export const register= async (req,res) => {}
+export const register= async (req,res) => {
+    const {email,password}=req.body;
+
+    try {
+        const existingUser= await db.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if(existingUser){
+            return res.status(400).json({
+                error:'User already exist'
+            })
+        }
+
+        const hashedPassword= await bcrypt.hash(password,10)
+
+        const newUser= await db.user.create({
+            data:{
+                email,
+                password:hashedPassword,
+                name,
+                role: UserRole.USER
+            }
+        })
+
+        const token= jwt.sign(
+            {userId:user.id},
+            process.env.JWT_SECRET,
+            {expiresIn:'7d'}
+        )
+
+        res.cookie('jwt',token,{
+            httpOnly: true,
+            secure:process.env.NODE_ENV !== 'development',
+            sameSite:'strict',
+            maxAge: 7 * 24 * 60 * 60 *1000 //7d
+        })
+
+        res.status(201).json({
+            success: true,
+            message:'User created Successfully',
+            user:{
+                id:newUser.id,
+                email:newUser.email,
+                name:newUser.name,
+                role:newUser.role,
+                image:newUser.image,
+            }
+        })
+
+    } catch (error) {
+        console.error('Error creating User',error)
+        res.status(500).json({
+            error:'Error creating User'
+        })
+    }
+}
 
 export const login= async (req,res) => {}
 
