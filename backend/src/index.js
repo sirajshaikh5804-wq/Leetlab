4984e52662ae72548702ca4a60c6c6fad031b9fe
sirajshaikh-dev <ss1425306@gmail.com>
2025-07-26 11:12:15 +0530

    initialize backend with Express and Prisma setup with docker

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+//index.js
+import express from 'express';
+import dotenv from 'dotenv';    
+
+dotenv.config(); 
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port 8080');
+});
