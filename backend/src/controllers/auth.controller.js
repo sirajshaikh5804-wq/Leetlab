6e662bd0c43f348fd341ebed9bc812e4fe4aa009
sirajshaikh-dev <ss1425306@gmail.com>
2025-07-26 12:58:56 +0530

    implement login functionality with JWT authentication and error handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -64,7 +64,59 @@ export const register= async (req,res) => {
     }
 }
 
-export const login= async (req,res) => {}
+export const login= async (req,res) => {
+    const {email,password}=req.body
+
+    try {
+        const user= await db.user.findUnique({
+            where:{email}
+        })
+
+        if(!user){
+            return res.status(401).json({
+                error:'User not found'
+            })
+        } 
+
+        const isMatch= await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return res.status(401).json({
+                error:'Invalid credentials'
+            })
+        }
+
+        const token= jwt.sign(
+            {id:user.id},
+            process.env.JWT_SECRET,
+            {expiresIn:'7d'}
+        )
+        res.cookie('jwt',token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV !=='development',
+            sameSite:"strict",
+            maxAge: 7*24*60*60*1000 //7d
+        })
+
+        res.status(201).json({
+            success:true,
+            message:'User logged in Successfully',
+            user:{
+                id:user.id,
+                email:user.email,
+                name:user.name,
+                role:user.role,
+                image:user.image
+            }
+        })
+
+    } catch (error) {
+        console.error('Error login in user',error)
+        res.status(500).json({
+            error:'Error login user'
+        })
+    }
+}
 
 export const logout= async (req,res) => {}
 
