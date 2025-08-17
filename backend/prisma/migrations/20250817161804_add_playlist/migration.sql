bf2a35d46f57c41bbed1274bb846e48b2c6482ac
sirajshaikh-dev <ss1425306@gmail.com>
2025-08-17 22:14:15 +0530

    add playlist schema

---
+++ backend/prisma/migrations/20250817161804_add_playlist/migration.sql
@@ -0,0 +1,40 @@
+-- AlterTable
+ALTER TABLE "Submission" ALTER COLUMN "updatedAt" DROP DEFAULT;
+
+-- CreateTable
+CREATE TABLE "Playlist" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Playlist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "problemsInPlaylist" (
+    "id" TEXT NOT NULL,
+    "playlistId" TEXT NOT NULL,
+    "problemId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "problemsInPlaylist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Playlist_name_userId_key" ON "Playlist"("name", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "problemsInPlaylist_playlistId_problemId_key" ON "problemsInPlaylist"("playlistId", "problemId");
+
+-- AddForeignKey
+ALTER TABLE "Playlist" ADD CONSTRAINT "Playlist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "problemsInPlaylist" ADD CONSTRAINT "problemsInPlaylist_playlistId_fkey" FOREIGN KEY ("playlistId") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "problemsInPlaylist" ADD CONSTRAINT "problemsInPlaylist_problemId_fkey" FOREIGN KEY ("problemId") REFERENCES "Problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
