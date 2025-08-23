4c001cd8ceb18300302c33aa9336e7a908437929
sirajshaikh-dev <ss1425306@gmail.com>
2025-08-23 20:22:35 +0530

    add: implement LoginForm component and integrate it into LoginPage layout

--- frontend/src/page/LoginPage.jsx
+++ frontend/src/page/LoginPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import LoginForm from '../components/LoginForm'
+import AuthImagePattern from '../components/AuthImagePattern'
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='h-screen grid lg:grid-cols-2'>
+      <LoginForm/>
+
+      {/* Right Side - Image/Pattern */}
+      <AuthImagePattern
+        title={"Welcome Back!"}
+        subtitle={
+          "Sign in to continue your journey with us. Don't have an account? Create now."
+        }
+      />
+    </div>
   )
 }
 
