a1bc7038b6acbbe7484cc3063f5798af561076a6
sirajshaikh-dev <ss1425306@gmail.com>
2025-09-09 21:02:45 +0530

    feat: implement CreateProblemForm component with form validation using Zod and React Hook Form

--- frontend/src/components/CreateProblemForm.jsx
+++ frontend/src/components/CreateProblemForm.jsx
@@ -1,9 +1,96 @@
 import React from 'react'
+import { useForm, useFieldArray, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod"
+import {
+  Plus,
+  Trash2,
+  Code2,
+  FileText,
+  Lightbulb,
+  BookOpen,
+  CheckCircle2,
+  Download,
+} from "lucide-react";
+import Editor from "@monaco-editor/react";
+import { useState } from 'react';
+import { axiosInstance } from "../lib/axios"
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+const problemSchema = z.object({
+  title: z.string().min(3, "Title must be at least 3 characters"),
+  description: z.string().min(10, "Description must be at least 10 characters"),
+  difficulty: z.enum(["EASY", "MEDIUM", "HARD"]),
+  tags: z.array(z.string()).min(1, "At least one tag is required"),
+  constraints: z.string().min(1, "Constraints are required"),
+  hints: z.string().optional(),
+  editorial: z.string().optional(),
+  testcases:
+    z.array(
+      z.object({
+        input: z.string().min(1, "Input is required"),
+        output: z.string().min(1, "Output is required"),
+      })
+    )
+    .min(1, "At least one test case is required"),
+  examples: z.object({
+    JAVASCRIPT: z.object({
+      input: z.string().min(1, "Input is required"),
+      output: z.string().min(1, "Output is required"),
+      explanation: z.string().optional(),
+    }),
+    PYTHON: z.object({
+      input: z.string().min(1, "Input is required"),
+      output: z.string().min(1, "Output is required"),
+      explanation: z.string().optional(),
+    }),
+    JAVA: z.object({
+      input: z.string().min(1, "Input is required"),
+      output: z.string().min(1, "Output is required"),
+      explanation: z.string().optional(),
+    }),
+  }),
+  codeSnippets: z.object({
+    JAVASCRIPT: z.string().min(1, "JavaScript code snippet is required"),
+    PYTHON: z.string().min(1, "Python code snippet is required"),
+    JAVA: z.string().min(1, "Java solution is required"),
+  }),
+  referenceSolutions: z.object({
+    JAVASCRIPT: z.string().min(1, "JavaScript solution is required"),
+    PYTHON: z.string().min(1, "Python solution is required"),
+    JAVA: z.string().min(1, "Java solution is required"),
+  }),
+});
+
 
 const CreateProblemForm = () => {
-  return (
-    <div>CreateProblemForm</div>
+  const navigation = useNavigate()
+  const { register, control, handleSubmit, reset, formState: { errors } } = useForm(
+    {
+      resolver: zodResolver(problemSchema),
+      defaultValues: {
+        testcases: [{ input: "", output: "" }],
+        tags: [""],
+        examples: {
+          JAVASCRIPT: { input: "", output: "", explanation: "" },
+          PYTHON: { input: "", output: "", explanation: "" },
+          JAVA: { input: "", output: "", explanation: "" },
+        },
+        codeSnippets: {
+          JAVASCRIPT: "function solution() {\n  // Write your code here\n}",
+          PYTHON: "def solution():\n    # Write your code here\n    pass",
+          JAVA: "public class Solution {\n    public static void main(String[] args) {\n        // Write your code here\n    }\n}",
+        },
+        referenceSolutions: {
+          JAVASCRIPT: "// Add your reference solution here",
+          PYTHON: "# Add your reference solution here",
+          JAVA: "// Add your reference solution here",
+        },
+      }
+    }
   )
 }
+
 
 export default CreateProblemForm
